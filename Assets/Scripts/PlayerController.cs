a7b7fb530cd541c3c4ccaba252b8a6829bbf82c0
Nesreen Ismail <nesreeni@umich.edu>
2025-10-08 00:52:39 -0400

    Revert "Village Prototype, Level Design"
    This reverts commit 0d15ae4e084c1db4c2b3ca7801e45cdc55806174.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,46 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class ParallaxLayer : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public Camera cam;
-    public Transform followTarget;
+    public float runSpeed = 5f;
+    Vector2 moveInput;
 
-    private Vector3 startingPosition;
-    private float startingZ;
+    public bool IsMoving { get; private set; }
 
-    // How far the camera has moved since the start (in 2D space)
-    private Vector2 CamMoveSinceStart => (Vector2)cam.transform.position - (Vector2)startingPosition;
+    Rigidbody2D rb;
 
-    // Distance between this layer and the follow target (for parallax)
-    private float ZDistanceFromTarget => transform.position.z - followTarget.position.z;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-    // Clipping plane calculation (used to normalize parallax)
-    private float ClippingPlane => Mathf.Abs(cam.transform.position.z) + (ZDistanceFromTarget > 0 ? cam.farClipPlane : cam.nearClipPlane);
-
-    // Parallax multiplier based on depth
-    private float ParallaxFactor => Mathf.Abs(ZDistanceFromTarget) / ClippingPlane;
-
+  
     void Start()
     {
-        if (cam == null) cam = Camera.main;
-        startingPosition = transform.position;
-        startingZ = transform.position.z;
+        
     }
 
-    void LateUpdate()
+   
+    void Update()
     {
-        if (cam == null || followTarget == null) return;
+        
+    }
 
-        Vector2 newPos2D = (Vector2)startingPosition + CamMoveSinceStart * ParallaxFactor;
-        transform.position = new Vector3(newPos2D.x, newPos2D.y, startingZ);
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * runSpeed, rb.linearVelocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+        
     }
 }
-
-
-
-
-
-
-
