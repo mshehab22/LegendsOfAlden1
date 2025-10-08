30a72471ad38a5f4a75a1e6d6684c7b09cba640b
mshehab22 <m.shehab2000@hotmail.com>
2025-10-08 01:08:35 -0400

    1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,42 +1,181 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
+using System;
 
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+// TODO: Part 11 at 31:00 mentions wall climbing 
+
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerController : MonoBehaviour
 {
-    public float runSpeed = 5f;
-    Vector2 moveInput;
-
-    public bool IsMoving { get; private set; }
-
     Rigidbody2D rb;
+    Animator animator;
+    Vector2 moveInput;
+    TouchingDirections touchingDirections;
+
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public float airWalkSpeed = 3f;
+    public float jumpImpulse = 10f;
+    public bool _isFacingRight = true;
+
+
+    [SerializeField] private bool _isMoving = false;
+    [SerializeField] private bool _isRunning = false;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
-
-  
-    void Start()
-    {
-        
-    }
-
-   
-    void Update()
-    {
-        
+        animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput.x * runSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.linearVelocity.y);
+        animator.SetFloat(AnimationStrings.yVelocity, rb.linearVelocity.y);
+    }
+
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+        }
+    }
+
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+
+        private set
+        {
+            _isRunning = value;
+            animator.SetBool(AnimationStrings.isRunning, value);
+        }
+    }
+
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+            if (IsMoving && !touchingDirections.IsOnWall)
+            {
+                if (touchingDirections.IsGrounded)
+                {
+                    if (IsRunning)
+                    {
+                        return runSpeed;
+                    }
+                    else
+                    {
+                        return walkSpeed;
+                    }
+                }
+                else
+                {
+                    // Air move
+                    return airWalkSpeed;
+                }
+            }
+            else
+            {
+                return 0; // Idle speed is 0
+            }
+        }
+
+        set
+        {
+        }
+
+    }
+
+    public bool IsFacingRight
+    {
+        get { return _isFacingRight; }
+
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                // Flip the local scale to make the player face the opposite direction
+                transform.localScale *= new Vector2(-1, 1);
+            }
+
+            _isFacingRight = value;
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
 
-        IsMoving = moveInput != Vector2.zero;
-        
+        IsMoving = (moveInput != Vector2.zero);
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight) // Facing right direction
+        {
+            IsFacingRight = true;
+
+        }
+        else if (moveInput.x < 0 && IsFacingRight) // Facing left direction
+        {
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsRunning = true;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        // TODO add is Alive condition
+        if (context.started && touchingDirections.IsGrounded)
+        {
+            animator.SetTrigger(AnimationStrings.jumpTrigger);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpImpulse);
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            animator.SetTrigger(AnimationStrings.attackTrigger);
+        }
     }
 }
+
+
+
+
+
+
+
