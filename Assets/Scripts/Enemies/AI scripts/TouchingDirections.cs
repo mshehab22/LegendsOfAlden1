cbe6ff5ba39e71dce9c694b4c01e72d20172b03e
fatimatohfe0 <fatimatohfee2@gmail.com>
2025-10-08 17:35:04 -0400

    added enemy AI

---
+++ Assets/Scripts/Enemies/AI scripts/TouchingDirections.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Touching_Directions : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
